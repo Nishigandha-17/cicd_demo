d01dd24b87e08cb1d3c593d6789103937933e2c3
nishigandha bhor <nishigandhabhor02@gmail.com>
2025-03-03 23:35:07 +0530

    "fix: commit for checking deployment changes "

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ function App() {
         <p>
          Welcome to my 1st CICD Application. This is the first change in the code.😊😍
         </p>
+        <p>
+          <ul>
+            <li>1.create react app</li>
+            <li>2.create github repository</li>
+            <li>3.create Azure portal account</li>
+            <li>4.create App service</li>
+            <li>4.monitor github action</li>
+            <li>5.deploy the app</li>
+            <li>6.check the app</li>
+            <li>7.check the logs</li>
+            <li>8.check the deployment</li>
+          </ul>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
